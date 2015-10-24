5f15d9ce7361ff4c412910db36ee58ec3e5cc995
ywett02 <ivet.jurcikova@gmail.com>
2015-10-24 19:51:02 +0200

    updated HotelDaoIml

--- BookingManager/src/main/java/cz/muni/fi/pa165/bookingmanager/dao/HotelDaoImpl.java
+++ BookingManager/src/main/java/cz/muni/fi/pa165/bookingmanager/dao/HotelDaoImpl.java
@@ -4,15 +4,18 @@ import cz.muni.fi.pa165.bookingmanager.entity.Hotel;
 import java.util.Collections;
 import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceUnit;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Iveta Jurcikova <ivet.jurcikova@gmail.com>
  */
+@Repository
 public class HotelDaoImpl implements HotelDao {
-    
-     @PersistenceUnit
+
+    @PersistenceUnit
     private EntityManager em;
 
     @Override
@@ -20,22 +23,30 @@ public class HotelDaoImpl implements HotelDao {
         this.validateHotel(hotel);
         em.persist(hotel);
     }
-    
+
     @Override
     public Hotel findById(Long id) {
-        if(id==null) throw new IllegalArgumentException("Id is null");
+        if (id == null) {
+            throw new IllegalArgumentException("Id is null");
+        }
         return em.find(Hotel.class, id);
     }
-    
+
     @Override
     public Hotel findByName(String name) {
-        if(name==null||name.equals("")) throw new IllegalArgumentException("Name is not valid");
-        return em.createQuery("Select h from Hotel h where h.name=:name",Hotel.class).setParameter("name", name).getSingleResult();
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Name is not valid");
+        }
+        try {
+            return em.createQuery("Select h from Hotel h where h.name=:name", Hotel.class).setParameter("name", name).getSingleResult();
+        } catch (NoResultException nrf) {
+            return null;
+        }
     }
 
     @Override
     public List<Hotel> findAll() {
-        return Collections.unmodifiableList((em.createQuery("Select h from Hotel h",Hotel.class)).getResultList());
+        return Collections.unmodifiableList((em.createQuery("Select h from Hotel h", Hotel.class)).getResultList());
     }
 
     @Override
@@ -49,21 +60,25 @@ public class HotelDaoImpl implements HotelDao {
         this.validateHotel(hotel);
         em.remove(hotel);
     }
-    
+
     /**
-        Method for validating attributes of hotel
-        
-        @param hotel
-        @exception IllegalArgumentException if some attribute is not valid
-    **/
-    private void validateHotel(Hotel hotel){
-        if(hotel==null) throw new IllegalArgumentException("Hotel is null");
-        if(hotel.getId()!=null) throw new IllegalArgumentException("Hotel already exist");
-        if(hotel.getName()==null||hotel.getName().equals("")) throw new IllegalArgumentException("Name is not valid");
-        
+     * Method for validating attributes of hotel
+     *
+     * @param hotel
+     * @exception IllegalArgumentException if some attribute is not valid
+    *
+     */
+    private void validateHotel(Hotel hotel) {
+        if (hotel == null) {
+            throw new IllegalArgumentException("Hotel is null");
+        }
+        if (hotel.getId() != null) {
+            throw new IllegalArgumentException("Hotel already exist");
+        }
+        if (hotel.getName() == null || hotel.getName().equals("")) {
+            throw new IllegalArgumentException("Name is not valid");
+        }
+
     }
-    
-   
-   
-    
+
 }
