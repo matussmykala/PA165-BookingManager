b3afd64b7c94d698a0bd91cac06320adf75cd4fc
Vladimir Caniga <vladimircaniga@gmail.com>
2015-11-25 15:54:35 +0100

    - reorganized pom.xml files to use dependancyManagement - added some more methods to Customer api and service layers - changed testng annotations to junit anotations on HotelFacadeImplTest

--- api-layer/src/main/java/cz/muni/fi/pa165/bookingmanager/facade/CustomerFacade.java
+++ api-layer/src/main/java/cz/muni/fi/pa165/bookingmanager/facade/CustomerFacade.java
@@ -25,4 +25,34 @@ public interface CustomerFacade {
      * @return all customers
      */
     Collection<CustomerDTO> getAllCustomers();
+
+    /**
+     * Finds a customer based on his ID.
+     *
+     * @param customerId ID of the customer
+     * @return customer with specified ID
+     */
+    CustomerDTO findCustomerById(Long customerId);
+
+    /**
+     * Checks if the specified user has admin role.
+     *
+     * @param customer customer to be checked
+     * @return true if given customer has admin role, false if not
+     */
+    boolean isAdmin(CustomerDTO customer);
+
+    /**
+     * Updates an existing customer.
+     *
+     * @param customerDTO customer to be updated
+     */
+    void updateCustomer(CustomerDTO customerDTO);
+
+    /**
+     * Deletes a customer with given ID
+     *
+     * @param customerId ID of a customer that will be deleted
+     */
+    void deleteCustomer(Long customerId);
 }
