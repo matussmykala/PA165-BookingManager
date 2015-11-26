02bc0da86895a2782551af59166679142d54c5de
Iveta.Jurcikova <Iveta.Jurcikova@P0105.hcg.homecredit.net>
2015-11-26 11:06:51 +0100

    Added method FindAllFreeRoomsInHotel to HotelService

--- service-layer/src/main/java/cz/muni/fi/pa165/bookingmanager/service/HotelServiceImpl.java
+++ service-layer/src/main/java/cz/muni/fi/pa165/bookingmanager/service/HotelServiceImpl.java
@@ -7,11 +7,15 @@ package cz.muni.fi.pa165.bookingmanager.service;
 
 import cz.muni.fi.pa165.bookingmanager.entity.Hotel;
 import cz.muni.fi.pa165.bookingmanager.dao.HotelDao;
+import cz.muni.fi.pa165.bookingmanager.dao.ReservationDao;
 import cz.muni.fi.pa165.bookingmanager.dao.RoomDao;
+import cz.muni.fi.pa165.bookingmanager.dto.ReservationDTO;
+import cz.muni.fi.pa165.bookingmanager.entity.Reservation;
 import cz.muni.fi.pa165.bookingmanager.entity.Room;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,7 +32,10 @@ public class HotelServiceImpl implements HotelService{
     private HotelDao hotelDao;
     
     @Autowired
-    private RoomDao roomDao;
+    private RoomService roomService;
+    
+    @Autowired
+    private ReservationService reservationService;
     
     @Override
     public void createHotel(Hotel hotel) throws DataAccessException{
@@ -89,10 +96,36 @@ public class HotelServiceImpl implements HotelService{
 
     @Override
     public List<Room> findFreeRoomInRange(Hotel hotel,Date start, Date end) {
+          List<Reservation> reservationsInTime = new ArrayList<>();
+          List<Room> bookedRooms = new ArrayList<>();
+          List<Room> roomsOfHotel = new ArrayList<>();
+          List<Room> reservedRoomsOutOfHotel = new ArrayList<>();
+          List<Room> reservedRoomsOfHotel = new ArrayList<>();
+          List<Room> freeRoomsOfHotel = new ArrayList<>();
           
+          //we get all reservation in cercain time
+          reservationsInTime.addAll(reservationService.getReservationsOfTime(start, end));
           
+          //we get all reserved rooms
+          for (Reservation reservation : reservationsInTime) {
+              bookedRooms.add(reservation.getRoom());
+          }
+          //we get all rooms in hotel
+          roomsOfHotel.addAll(roomService.findAllRoomsByHotel(hotel));
           
-          return null;
+          //we get all reservedRoom which arent in hotel
+          reservedRoomsOutOfHotel.addAll(bookedRooms);
+          reservedRoomsOutOfHotel.removeAll(roomsOfHotel);
+          
+          //we get all reservedRoom in hotel
+          reservedRoomsOfHotel.addAll(bookedRooms);
+          reservedRoomsOfHotel.removeAll(reservedRoomsOutOfHotel);
+          
+          //we get all free rooms in hotel
+          freeRoomsOfHotel.addAll(roomsOfHotel);
+          freeRoomsOfHotel.removeAll(reservedRoomsOfHotel);
+          
+          return Collections.unmodifiableList(freeRoomsOfHotel);
           
          
          
