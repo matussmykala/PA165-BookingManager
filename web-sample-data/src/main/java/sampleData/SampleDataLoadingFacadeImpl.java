cc0f4d7cb54eb12adf78edd4bb2f901a02616469
Vladimir Caniga <vladimircaniga@gmail.com>
2015-12-13 13:37:20 +0100

    - added sample data for customer

--- web-sample-data/src/main/java/sampleData/SampleDataLoadingFacadeImpl.java
+++ web-sample-data/src/main/java/sampleData/SampleDataLoadingFacadeImpl.java
@@ -1,15 +1,20 @@
 package sampleData;
+
+import cz.muni.fi.pa165.bookingmanager.entity.Customer;
 import cz.muni.fi.pa165.bookingmanager.entity.Hotel;
+import cz.muni.fi.pa165.bookingmanager.service.CustomerService;
 import cz.muni.fi.pa165.bookingmanager.service.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author matus
@@ -18,26 +23,35 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional //transactions are handled on facade layer
 public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade
 {
+    private static final Logger log = Logger.getLogger(SampleDataLoadingFacadeImpl.class.getName());
+
     @Autowired
     HotelService hotelService;
-    
+
+    @Autowired
+    private CustomerService customerService;
+
     public static final String JPEG = "image/jpeg";
-     
+
     Date date;
 
     @Override
     public void loadData() throws IOException {
+        log.log(Level.WARNING, "load sample data method called!");
+
         this.createDate();
         Hotel hotel = hotel("Voronez","Brno","Luxusny hotel v Brne", "null", null, date);
+
+        Customer customer1 = customer("Janko", "Hraško", "j.hrasko@email.com", "jhrasko", "heslo123");
     }
-    
+
     private void createDate(){
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.MONTH, 1);
         calendar.set(Calendar.DATE, 5);
         this.date = calendar.getTime();
     }
-    
+
     private Hotel hotel(String name, String address, String description, String imageFile, String mimeType, Date date) throws IOException {
         Hotel hotel = new Hotel();
         hotel.setName(name);
@@ -48,8 +62,18 @@ public class SampleDataLoadingFacadeImpl implements SampleDataLoadingFacade
         hotelService.createHotel(hotel);
         return hotel;
     }
-    
-      private byte[] readImage(String file) throws IOException {
+
+    private Customer customer(String name, String surname, String email, String username, String password) {
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setSurname(surname);
+        customer.setEmail(email);
+        customer.setUsername(username);
+        customerService.registerCustomer(customer, password);
+        return customer;
+    }
+
+    private byte[] readImage(String file) throws IOException {
         try (InputStream is = this.getClass().getResourceAsStream("/" + file)) {
             int nRead;
             ByteArrayOutputStream buffer = new ByteArrayOutputStream();
