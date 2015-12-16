ccdadf5e807aed282d0cdb1f73d0c89080e0b1c5
ywett02 <ivet.jurcikova@gmail.com>
2015-12-16 09:38:22 +0100

    Added implementation of rest layer

--- rest-layer/src/main/java/cz/muni/fi/pa165/RootWebContext.java
+++ rest-layer/src/main/java/cz/muni/fi/pa165/RootWebContext.java
@@ -1,10 +1,16 @@
-package cz.muni.fi.pa165.rest.layer.config;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import cz.muni.fi.pa165.bookingmanager.dto.CustomerDTO;
+import cz.muni.fi.pa165.bookingmanager.dto.RoomDTO;
 import cz.muni.fi.pa165.bookingmanager.service.config.ServiceConfiguration;
 import cz.muni.fi.pa165.rest.layer.mixin.CustomerDTOMixin;
 import org.springframework.context.annotation.ComponentScan;
@@ -25,7 +31,7 @@ import sampleData.SampleDataConfiguration;
 
 @EnableWebMvc
 @Configuration
-@Import({ServiceConfiguration.class,SampleDataConfiguration.class})
+@Import({SampleDataConfiguration.class})
 @ComponentScan(basePackages = {"cz.muni.fi.pa165.rest.layer.controllers"})
 public class RootWebContext extends WebMvcConfigurerAdapter {
 
@@ -48,8 +54,8 @@ public class RootWebContext extends WebMvcConfigurerAdapter {
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH));
         
-
-        objectMapper.addMixIn(CustomerDTO.class, CustomerDTOMixin.class);
+        
+        
         
         objectMapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
    
---
+++ rest-layer/src/main/java/cz/muni/fi/pa165/rest/layer/controllers/RoomController.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.rest.layer.controllers;
+
+
+
+import cz.muni.fi.pa165.bookingmanager.dto.RoomDTO;
+import cz.muni.fi.pa165.bookingmanager.facade.RoomFacade;
+import cz.muni.fi.pa165.rest.layer.exceptions.ResourceAlreadyExistingException;
+import cz.muni.fi.pa165.rest.layer.exceptions.ResourceNotFoundException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import org.springframework.http.MediaType;
+
+/**
+ * REST Controller for Products
+ *
+ * @author brossi
+ */
+@RestController
+@RequestMapping("/room")
+public class RoomController {
+
+    
+    @Inject
+    private RoomFacade roomFacade;
+
+    /**
+     * Get list of Products curl -i -X GET
+     * http://localhost:8080/pa165/rest/room
+     *
+     * @return ProductDTO
+     */
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final List<RoomDTO> getRooms() {
+        return roomFacade.getAllRooms();
+    }
+
+    /**
+     *
+     * Get Product by identifier id curl -i -X GET
+     * http://localhost:8080/pa165/rest/room/1
+     *
+     * @param id identifier for a product
+     * @return ProductDTO
+     * @throws ResourceNotFoundException
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final RoomDTO getRoom(@PathVariable("id") long id) throws Exception {
+        RoomDTO roomDTO = roomFacade.getRoomById(id);
+        if (roomDTO != null) {
+            return roomDTO;
+        } else {
+            throw new ResourceNotFoundException();
+        }
+
+    }
+
+    /**
+     * Delete one product by id curl -i -X DELETE
+     * http://localhost:8080/pa165/rest/room/1
+     *
+     * @param id identifier for product
+     * @throws ResourceNotFoundException
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public final void deleteRoom(@PathVariable("id") long id) throws Exception {
+        try {
+            roomFacade.deleteRoom(id);
+        } catch (Exception ex) {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    /**
+     * Create a new product by POST method
+     * curl -X POST -i -H "Content-Type: application/json" --data 
+     * '{"name":"test","description":"test","color":"UNDEFINED","price":"200",
+     * "currency":"CZK", "categoryId":"1"}' 
+     * http://localhost:8080/pa165/rest/room/create
+     * 
+     * @param room roomCreateDTO with required fields for creation
+     * @return the created product RoomDTO
+     * @throws ResourceAlreadyExistingException
+     */
+    @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public final RoomDTO createRoom(@RequestBody RoomDTO roomDTO) throws Exception {
+
+        try {
+            roomFacade.createRoom(roomDTO);
+            return roomFacade.getRoomById(roomDTO.getId());
+        } catch (Exception ex) {
+            throw new ResourceAlreadyExistingException();
+        }
+    }
+}
+
+   
+   
