bb5425a1bed419370ac3861d5b2c90f790c8f8eb
Martin Cuchran <cuchy92@gmail.com>
2016-01-26 14:57:54 +0100

    Create rest-test.sh

---
+++ rest-test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# init
+
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+function pause(){
+	printf "\n\n${YELLOW}Press [Enter] to continue...${NC}\n\n" && read
+}
+
+printf "${GREEN}To show all rooms${NC}\n\n"
+    curl -i -X GET http://localhost:8080/pa165/rest/room
+
+pause
+
+printf "${GREEN}To get room with id (in our example, id=1)${NC}\n\n"
+    curl -i -X GET http://localhost:8080/pa165/rest/room/1
+
+pause
+
+printf "${GREEN}To delete room with id (in our example, id =1)${NC}\n\n"
+    curl -i -X DELETE http://localhost:8080/pa165/rest/room/1
+
+pause
+
+printf "${GREEN}Check that room is gone${NC}\n\n"
+    curl -i -X GET http://localhost:8080/pa165/rest/room/1
+
+pause
+
+printf "${GREEN}To create room(name, numberOfBeds, price, hotel, reservations) (in out example name = TEST, numberOfBeds = 2, price = 25.20, hotel = getHotel(id=2), reservations = [])${NC}\n\n"
+    curl -X POST -i -H "Content-Type: application/json" --data '{"name":"TEST","numberOfBeds":2,"price":25.20,"hotel":{"id":2,"name":"Park Hotel","address":"Praha","description":"Pekny hotel v Prahe","lastUpdateDay":"2016-07-27 00:00"},"reservations":[]}' http://localhost:8080/pa165/rest/room/create
+
+pause
+
+printf "${GREEN}Check that room was really created${NC}\n\n"
+    curl -i -X GET http://localhost:8080/pa165/rest/room/6
+pause
